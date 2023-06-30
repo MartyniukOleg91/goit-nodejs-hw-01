dc2c8355056b15cd8aaf0214147cad73c7aebe8b
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-06-30 13:01:39 +0300

    1

--- contact.js
+++ contact.js
@@ -1,6 +1,7 @@
 const { isUtf8 } = require("buffer");
 const fs = require("fs/promises");
 const path = require("path");
+const { v4 } = require("uuid");
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
@@ -19,6 +20,23 @@ const getById = async (id) => {
   return result;
 };
 
-const add = async () => {};
+const add = async (data) => {
+  const products = await getAll();
+  const newProduct = { ...data, id: v4() };
+  products.push(newProduct);
+  await fs.writeFile(contactsPath, JSON.stringify(products));
+  return newProduct;
+};
 
-module.exports = { getAll, getById };
+const updateById = async (id, data) => {
+  const products = await getAll();
+  const idx = products.findIndex((item) => item.id === id);
+  if (idx === -1) {
+    return null;
+  }
+  products[idx] = { ...data, id };
+  await fs.writeFile(contactsPath, JSON.stringify(products));
+  return products[idx];
+};
+
+module.exports = { getAll, getById, add, updateById };
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const invokeAction = async ({ action, id, data }) => {
   switch (action) {
     case "getAll":
       const products = await productOperations.getAll();
-
+      console.log(products);
       break;
     case "getById":
       const product = await productOperations.getById(id);
@@ -14,12 +14,38 @@ const invokeAction = async ({ action, id, data }) => {
       }
       console.log(product);
       break;
+    case "add":
+      const newProduct = await productOperations.add(data);
+
+    case "updateById":
+      const updateProduct = await productOperations.updateById(id, data);
+      if (!updateProduct) {
+        throw new Error(`Product with id ${id} not found`);
+      }
+      console.log(updateProduct);
+      break;
     default:
       console.log("Uncnow action");
   }
 };
 
-invokeAction({ action: "getAll" });
+// invokeAction({ action: "getAll" });
 
-const id = "AeHIrLTr6JkxGE6SN-0Rw";
-invokeAction({ action: "getById", id });
+// const id = "AeHIrLTr6JkxGE6SN-0Rw";
+// invokeAction({ action: "getById", id });
+
+// const newData = {
+//   id: "1",
+//   name: "Martyniuk Oleh ",
+//   email: "nulla.ante@vestibul.co.uk",
+//   phone: "(097) 144-4167",
+// };
+// invokeAction({ action: "add", data: newData });
+
+const updateId = "76fe8d68-5916-4e6a-8add-76dc9b44e5b2";
+const updateData = {
+  name: "Martyn",
+  email: "nulla",
+  phone: "(099) 156-4320",
+};
+invokeAction({ action: "updateById", id: updateId, data: updateData });
